2b519ba923677b926a7dc988a0ff3f22610f1469
hurlatunde <hurlatunde@gmail.com>
2021-09-09 19:06:48 +0100

    Revert "structure changes"
    This reverts commit f06eabaca56ed381f103858079d9331e37975dc5.

--- src/SDK/FsiEngineSDK.php
+++ src/SDK/FsiEngineSDK.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace FsiEngine\SDK;
-
 use \FsiEngine\Constants\Meta;
 use FsiEngine\Providers\AfricasTalkingProvider\AfricasTalkingIndex;
 use FsiEngine\Providers\Ecobank\EcobankIndex;
@@ -9,7 +7,7 @@ use FsiEngine\Providers\FCMB\FCMBIndex;
 use FsiEngine\Providers\SterlingBankProvider\SterlingBankIndex;
 use FsiEngine\Providers\WovenFinance\WovenFinanceIndex;
 
-class FsiEngine
+class FsiEngineSDK
 {
 
     /**
@@ -17,39 +15,29 @@ class FsiEngine
      * @var $developmentType
      * @var $developmentURL
      */
-    protected static $api_key;
-    protected static $developmentType;
-    protected static $developmentURL;
+    protected $api_key;
+    protected $developmentType;
+    protected $developmentURL;
 
     /**
      * @param null $api_key
      * @param string $deployment_type
      */
-    public function __construct($developmentURL = Meta::SANDBOX_URL)
-    { //set live as default...
-//        self::api_key = $api_key;
-
-        //double check here to see it app key set
-
+    public function __construct($api_key, $developmentType, $developmentURL = Meta::SANDBOX_URL){ //set live as default...
+        $this->api_key = $api_key;
+        $this->developmentType = $developmentType;
         $this->developmentURL = $developmentURL;
-        if (self::$developmentType === Meta::TESTING_DEPLOYMENT_TYPE) {
+        if($developmentType === Meta::TESTING_DEPLOYMENT_TYPE){
             $this->developmentURL = Meta::LIVE_BASE_URL;
         }
     }
 
-    public static function init($api, $type)
-    {
-        self::$api_key = $api;
-        self::$developmentType = $type;
-    }
-
     /**
      * =====================
      * @return object
      */
-    static public function AfricansTalkingProvider()
-    {
-        $provider = new AfricasTalkingIndex(self::$api_key, self::$developmentURL);
+    public function processAfricasTalkingProvider() {
+        $provider =  new AfricasTalkingIndex($this->api_key, $this->developmentURL);
         return $provider->providerServices();
     }
 
@@ -57,19 +45,8 @@ class FsiEngine
      * =====================
      * @return object
      */
-    static public function SterlingBankProvider()
-    {
-        $provider = new SterlingBankIndex(self::$api_key, self::$developmentURL);
-        return $provider->providerServices();
-    }
-
-
-    /**
-     * @return object
-     */
-    static public function FCMBProvider()
-    {
-        $provider = new FCMBIndex(self::$api_key, self::$developmentURL);
+    public function processSterlingBankProvider() {
+        $provider = new SterlingBankIndex($this->api_key, $this->developmentURL);
         return $provider->providerServices();
     }
 
@@ -77,9 +54,17 @@ class FsiEngine
      * =====================
      * @return object
      */
-    static public function EcobankProvider()
-    {
-        $provider = new EcobankIndex(self::$api_key, self::$developmentURL);
+    public function processFCMBProvider() {
+        $provider = new FCMBIndex($this->api_key, $this->developmentURL);
+        return $provider->providerServices();
+    }
+
+    /**
+     * =====================
+     * @return object
+     */
+    public function processEcobankProvider() {
+        $provider = new EcobankIndex($this->api_key, $this->developmentURL);
         return $provider->providerServices();
     }
 
@@ -87,9 +72,9 @@ class FsiEngine
      * ====================
      * @return object
      */
-    static public function WovenFinanceProvider()
+    public function processWovenFinanceProvider()
     {
-        $provider = new WovenFinanceIndex(self::$api_key, self::$developmentURL);
+        $provider = new WovenFinanceIndex($this->api_key, $this->developmentURL);
         return $provider->providerServices();
     }
 
